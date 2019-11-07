115d3c0bbfb16c3328f4d7d9af462a82743b6e18
Ana Karolina <anakarolina1966@icloud.com>
2019-11-07 20:08:18 -0300

    edição de Controllers

--- Desafio/Controllers/CompanyController.cs
+++ Desafio/Controllers/CompanyController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using Desafio.Repository;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +13,26 @@ namespace Desafio.Controllers
     [ApiController]
     public class CompanyController : ControllerBase
     {
+        private readonly CompanyRepository _companyRepository;
+        private readonly IMapper _mapper;
+
+        public CompanyController(CompanyRepository companyRepository, IMapper mapper)
+        {
+            _companyRepository = companyRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            return Ok(_companyRepository.GetAll());
+
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult Get(int Id)
+        {
+            return Ok(_companyRepository.GetById(Id));
+        }
     }
 }
--- Desafio/Controllers/PageController.cs
+++ Desafio/Controllers/PageController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using Desafio.Repository;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +13,26 @@ namespace Desafio.Controllers
     [ApiController]
     public class PageController : ControllerBase
     {
+        private readonly PageRepository _pageRepository;
+        private readonly IMapper _mapper;
+
+        public PageController(PageRepository pageRepository, IMapper mapper)
+        {
+            _pageRepository = pageRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            return Ok(_pageRepository.GetAll());
+
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult Get(int Id)
+        {
+            return Ok(_pageRepository.GetById(Id));
+        }
     }
 }
